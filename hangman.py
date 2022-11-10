3eb91bc259121847241b4784d5bf6d37ae597063
Justin Davis <justin4480@gmail.com>
2022-11-10 23:17:13 +0000

    black formatting

--- hangman.py
+++ hangman.py
@@ -2,34 +2,45 @@
 import random
 import os
 
+
 class Hangman(object):
 
-    alphabet = list('abcdefghijklmnopqrstuvwxyz')
+    alphabet = list("abcdefghijklmnopqrstuvwxyz")
 
     def __init__(self, secret_word: str, guesses: int = 7):
         self.secret_word = secret_word
-        self.avalible_guess = list('abcdefghijklmnopqrstuvwxyz')
+        self.avalible_guess = list("abcdefghijklmnopqrstuvwxyz")
         self.totalguesses = guesses
         self.remainingguesses = guesses
 
     def get_secret_grid(self) -> str:
-        return ' '.join([letter if letter.lower() not in self.avalible_guess else '_' for letter in self.secret_word])
+        return " ".join(
+            [
+                letter if letter.lower() not in self.avalible_guess else "_"
+                for letter in self.secret_word
+            ]
+        )
 
     def get_remaining_letters(self) -> str:
-        return ' '.join([letter if letter in self.avalible_guess else '_' for letter in self.alphabet])
+        return " ".join(
+            [
+                letter if letter in self.avalible_guess else "_"
+                for letter in self.alphabet
+            ]
+        )
 
     def get_remaining_guesses(self) -> str:
-        return f'{self.remainingguesses} of {self.totalguesses} guesses remaining'
+        return f"{self.remainingguesses} of {self.totalguesses} guesses remaining"
 
     def display_board(self) -> None:
-        os.system('clear')
-        print(self.get_secret_grid(), '\n')
-        print(self.get_remaining_letters(), '\n')
-        print(self.get_remaining_guesses(), '\n')
+        os.system("clear")
+        print(self.get_secret_grid(), "\n")
+        print(self.get_remaining_letters(), "\n")
+        print(self.get_remaining_guesses(), "\n")
 
     def get_player_guess(self):
         while True:
-            letter = input('Guess a letter: ')
+            letter = input("Guess a letter: ")
             if letter.lower() not in self.avalible_guess:
                 print("Not an appropriate choice.")
                 continue
@@ -39,7 +50,10 @@ class Hangman(object):
             break
 
     def check_for_win(self) -> bool:
-        return len(set(self.avalible_guess).intersection(set(self.secret_word.lower()))) == 0
+        return (
+            len(set(self.avalible_guess).intersection(set(self.secret_word.lower())))
+            == 0
+        )
 
     def check_for_loose(self) -> bool:
         return self.remainingguesses == 0 and not self.check_for_win()
@@ -50,23 +64,39 @@ class Hangman(object):
             self.get_player_guess()
             self.display_board()
             if self.check_for_win():
-                print('You win!', '\n')
+                print("You win!", "\n")
                 break
             if self.check_for_loose():
-                print('You lose!', '\n')
+                print("You lose!", "\n")
                 break
-            
+
 
 def main():
     while True:
         movies = [
-            "Alice in Wonderland","Toy Story 3","Cars 2","Winnie the Pooh","The Muppets",
-            "Wreck-It Ralph","Monsters University","Frozen","Big Hero 6","Inside Out","The Good Dinosaur",
-            "Moana","Beauty and the Beast","Coco","Incredibles 2","Aladdin","The Lion King"]
+            "Alice in Wonderland",
+            "Toy Story 3",
+            "Cars 2",
+            "Winnie the Pooh",
+            "The Muppets",
+            "Wreck-It Ralph",
+            "Monsters University",
+            "Frozen",
+            "Big Hero 6",
+            "Inside Out",
+            "The Good Dinosaur",
+            "Moana",
+            "Beauty and the Beast",
+            "Coco",
+            "Incredibles 2",
+            "Aladdin",
+            "The Lion King",
+        ]
         hangman = Hangman(random.choice(movies))
         hangman.run_game()
-        if input('Press enter to play again: ') != '':
+        if input("Press enter to play again: ") != "":
             break
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
