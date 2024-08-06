9e1a9ca59be58e093644d7dbbbb49f635503d4ef
Justin Davis <justin4480@gmail.com>
2024-08-06 22:44:09 +0100

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -3,5 +3,6 @@ sudo apt install xserver-xorg xinit
 sudo apt install libpangocairo-1.0-0
 sudo apt install python3-pip python3-xcffib python3-cairocffi
 sudo apt install pipx vim git sddm alacritty
-pipx qtile
-sudo wget https://raw.githubusercontent.com/qtile/qtile/master/resources/qtile.desktop /usr/share/xsessions
+pipx install qtile
+pipx ensurepath
+sudo wget https://raw.githubusercontent.com/qtile/qtile/master/resources/qtile.desktop -P /usr/share/xsessions/
