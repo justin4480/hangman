3854b8d231199aa4cb43d046e6fb2cab896a4960
Justin Davis <justin4480@gmail.com>
2024-08-06 22:59:14 +0100

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -6,3 +6,10 @@ sudo apt install pipx vim git sddm alacritty
 pipx install qtile
 pipx ensurepath
 sudo wget https://raw.githubusercontent.com/qtile/qtile/master/resources/qtile.desktop -P /usr/share/xsessions/
+
+# Install Brave
+sudo apt install curl
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser
